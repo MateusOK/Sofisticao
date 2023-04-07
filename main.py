1e5309c79273bd88e6955a569c3072e8f22d4073
3º DSM <dsm-3@fatec.academico>
2023-04-06 21:11:41 -0300

    feat: initial email input

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 root = Tk()
 
@@ -29,13 +31,19 @@ logo = PhotoImage(file="assets\dog_logo.png")
 left_label = Label(frame, image=left_image)
 right_label = Label(frame, image=right_image)
 logo_label = Label(frame, image=logo)
+title = ttk.Label(frame, text="Sofisticão", font="Nunito 35 bold", bootstyle="success")
+email_input = ttk.Entry(frame, bootstyle=SUCCESS, width=25, font="Arial 18")
+email_text = ttk.Label(frame, text="Email", font="Nunito 12 bold", bootstyle=SUCCESS)
 
 # Crie um rótulo para o texto
 text_label = Label(frame, text="Meu texto aqui")
 
 # Posicione as imagens e o texto na janela
 left_label.pack(side=LEFT)
-logo_label.place(x=600)# adicione um espaço de 10 pixels entre o texto e as imagens
+logo_label.place(x=565, y=140) # adicione um espaço de 10 pixels entre o texto e as imagens
 right_label.pack(side=RIGHT)
+email_input.place(x=465, y=400)
+title.place(x=520, y=300)
+email_text.place(x=470, y=370)
 
 root.mainloop()
