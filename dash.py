c1c5776e4bfb9ddd82dca0d20b15cbaa9a738d58
MateusOK <mateusribeiro-2004@hotmail.com>
2023-04-07 15:38:45 -0300

    feat: add exit button

--- dash.py
+++ dash.py
@@ -20,6 +20,8 @@ side_image = ctk.CTkImage(light_image=Image.open("assets\\side_line.png"))
 dash_logo = ctk.CTkImage(light_image=Image.open("assets\\dashboard_logo_white.png"))
 client_logo = ctk.CTkImage(light_image=Image.open("assets\\client_logo.png"))
 services_logo = ctk.CTkImage(light_image=Image.open("assets\\services_logo.png"))
+animals_logo = ctk.CTkImage(light_image=Image.open("assets\\animals_logo.png"))
+exit_logo = ctk.CTkImage(light_image=Image.open("assets\\exit_logo.png"))
 
 #labels
 # logo_label = ctk.CTkLabel(frame, image=logo_image)
@@ -29,18 +31,30 @@ services_logo = ctk.CTkImage(light_image=Image.open("assets\\services_logo.png")
 #buttons
 dash_button = ctk.CTkButton(root, text="Dashboard", image=dash_logo, fg_color="#27AE60",
                             corner_radius=5, height=35, width=180, bg_color="#F2F2F2")
-client_button = ctk.CTkButton(root, text="    Clientes", text_color="black", image=client_logo, corner_radius=7, 
+client_button = ctk.CTkButton(root, text="       Clientes", text_color="black", image=client_logo, corner_radius=7, 
                               fg_color="white", height=35, width=180, bg_color="#E0E0E0")
-services_button  = ctk.CTkButton(root, image=services_logo, text="  Serviços", text_color="black", corner_radius=7,
+services_button  = ctk.CTkButton(root, image=services_logo, text="      Serviços", text_color="black", corner_radius=7,
                                   fg_color="white", height=35, width=180, bg_color="#E0E0E0")
+animals_button  = ctk.CTkButton(root, image=animals_logo, text="        Animais", text_color="black", corner_radius=7,
+                                  fg_color="white", height=35, width=180, bg_color="#E0E0E0")
+exit_button  = ctk.CTkButton(root, image=exit_logo, text="  Sair", text_color="black", corner_radius=7,
+                                  fg_color="white", height=35, width=180, bg_color="#E0E0E0", compound="right")
 
 
-# # #posicionamento na tela
+
+
+
+
+#posicionamento na tela
+
 # logo_label.place(x=60, y=20)
 # side_line_label.place(x=15,y=300)
-services_button.place(x=40, y=400)
 dash_button.place(x=40, y=300)
 client_button.place(x=40, y=350)
+services_button.place(x=40, y=400)
+animals_button.place(x=40, y=450)
+exit_button.place(x=40, y=600)
+
 
 
 
