8e425633d48030e969eebe2f7a45eb2b6c04d910
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-07 13:37:58 -0300

    feat: left structure - container

--- dashContainer.py
+++ dashContainer.py
@@ -15,14 +15,45 @@ container = ctk.CTkFrame(root, bg_color="white", width=790,
 container.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 
-# logo image
+# logo - image
 logo_image = ctk.CTkImage(light_image=Image.open(
     "assets\dashboard__container\logo_container.png"), size=(50, 50))
-logo_label = ctk.CTkLabel(root, text="", image=logo_image)
+logo_label = ctk.CTkLabel(root, text="", image=logo_image, fg_color="white")
 logo_label.place(relx=0.22, rely=0.46, anchor=tkinter.CENTER)
 
 
-# title at container
-animal_title = ctk.CTkLabel(root, text="Nome do animal")
-animal_title.place(relx=0.28, rely=0.45, anchor=tkinter.CENTER)
+# title - container
+animal_title = ctk.CTkLabel(root, text="Nome do animal", font=ctk.CTkFont(
+    family="Open Sans", size=16, weight="bold"), fg_color="white", text_color="#292929")
+animal_title.place(relx=0.295, rely=0.449, anchor=tkinter.CENTER)
+
+# subtitle - container
+owner_title = ctk.CTkLabel(root, text="Nome do dono", font=ctk.CTkFont(
+    family="Open Sans", size=16, weight="bold"), fg_color="white", text_color="#6A6A6A")
+owner_title.place(relx=0.292, rely=0.478, anchor=tkinter.CENTER)
+
+
+# status - container
+status_text = ctk.CTkLabel(root, text="Status: ", font=ctk.CTkFont(
+    family="Open Sans", size=16, weight="bold"), fg_color="white", text_color="#292929")
+animal_title.place(relx=0.295, rely=0.449, anchor=tkinter.CENTER)
+
+status_text.place(relx=0.225, rely=0.55, anchor=tkinter.CENTER)
+
+# in progress - status - container
+in_progress_text = ctk.CTkLabel(root, text="em andamento", font=ctk.CTkFont(
+    family="Open Sans", size=16, weight="bold"), fg_color="white", text_color="#F2994A")
+in_progress_text.place(relx=0.293, rely=0.55, anchor=tkinter.CENTER)
+
+# owner - container
+owner_text = ctk.CTkLabel(root, text="Dono: ", font=ctk.CTkFont(
+    family="Open Sans", size=16, weight="bold"), fg_color="white", text_color="#292929")
+owner_text.place(relx=0.224, rely=0.580, anchor=tkinter.CENTER)
+
+# waiting - owner - container
+waiting_text = ctk.CTkLabel(root, text="Aguardando", font=ctk.CTkFont(
+    family="Open Sans", size=16, weight="bold"), fg_color="white", text_color="#F2994A")
+waiting_text.place(relx=0.285, rely=0.580, anchor=tkinter.CENTER)
+
+
 root.mainloop()
