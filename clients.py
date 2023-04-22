bb4da3e70b28373663770d94a20de3df3b34e9d0
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-22 14:15:24 -0300

    feat: button and search for clients

--- clients.py
+++ clients.py
@@ -16,9 +16,24 @@ def clientsWindow():
                          txt_color="white", fg_color="#FFA826")
     menuContainer.grid(row=0, column=0)
 
-    ################ POSTER  ###############
+   # Search bar
 
-    # Dashboard - Title
+    search_logo = ctk.CTkImage(light_image=Image.open(
+        "assets\\client__page\\search.png"))
+
+    search = ctk.CTkEntry(root, bg_color="white", border_color="#C9C9C9", fg_color="white",
+                          placeholder_text="Cleiton Rasta", text_color="black", width=485, height=48)
+    search.place(x=505, y=70)
+
+    btn_search = ctk.CTkButton(root, text="", image=search_logo, width=80, height=48,
+                               bg_color="white", fg_color="#FFA826", hover_color="#FF9D0A")
+    btn_search.place(x=1000, y=70)
+
+    results_text = ctk.CTkLabel(root, text="5 resultados", font=ctk.CTkFont(
+        family="Nunito", size=13, weight="bold"), fg_color="white", text_color="#A4A4A4")
+    results_text.place(x=1000, y=120)
+
+    # Scroll - Container
 
     title_poster = ctk.CTkLabel(root, text="Clientes", font=ctk.CTkFont(
         family="Nunito", size=20, weight="bold"), fg_color="white", text_color="black")
